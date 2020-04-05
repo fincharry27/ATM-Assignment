9f3af4e76f8f2798169abba333f8f2bb9d3c1c59
Oainientk3 <33579446+Oainientk3@users.noreply.github.com>
2020-04-05 06:26:54 +0100

    Add files via upload

--- ATMSimulator/Thread.cpp
+++ ATMSimulator/Thread.cpp
@@ -1,90 +1,108 @@
-/*
- * ThreadATM.c
- *
- *
- *      Author: Elliot Kinkead
- *      Account Number: 118118
- *      Pin Number: 0118
- *
- *      Description: Mainly Pesudocode, But mostly implemeted
- */
+#include <fstream>
 #include <iostream>
-#include <cstdlib>
+#include <string>
+#include <sstream>
 using namespace std;
+/*
+ * ThreadATM.cpp
+ *
+ *
+ *     	ATM Simulator just to get inital code working
+ *      Account Number: 118118
+ *      Pin Number: 1181
+ *
+ *      Description: It works
+ *      Todo: Check if this actually works or I need to rework this thing again
+ */
 
-Public int StoreAcNumber 118118;
-Public int StorePin 0118;
-Public int StoreBalance 50;
+class Thread
+{
+	int StoreAcNumber = 18118;
+	int StorePin = 1181;
+	int StoreBalance = 50;
+	int WithdrawAmmount;
+	int MenuMoney;
+		int Menu;
 
+public:
+	void StageAccountCheck(); //Enter Pin number and account number
+	void StageMenu(); //Main menu for ATM
+	void ReturnCard(); //Return Card
+	void AccountBalance(); //Check how poor you are sorry bank balance
 
-void StageAccountCheck()
+	//Todo: Implement below slightly better
+	void WithdrawMoney(); //Withdraw cash
+	void BalanceWithdraw(); //The actaul part where it withdraws your cash
+
+};
+
+void Thread::StageAccountCheck()
 {
 	int ACNumber; //Account Number
 	int Pin; // Pin Number
 	// Please enter account number and check if true or false. (Hard coded for now)
-
 	cout << "Please enter your account number";
 	cin >> ACNumber;
-
 	// Enter Pin number
-
 	cout << "Please enter your Pin Number";
 	cin >> Pin;
-
 	//Check if true or false.
-
-	if (ACNumber = StoreAcNumber && Pin = StorePin)
+	if (StoreAcNumber == ACNumber && StorePin == Pin)
 	{
 		cout << "Hello Your Name";
 		StageMenu();
-
 	}
 	else
 	{
 		cout << "Youve entered incorect account number or Pin Number Please try again";
 		StageAccountCheck();
 	}
-
-	// Rejected throw them to
-
-	// Correct yeet them to Stage2
-
 }
 
-void StageMenu()
+void Thread::StageMenu()
 {
-	int num;
-
+	//Main Menu for ATM
+	//TODO: Remove temp menu when GUI is Implemented
 	cout << "Please select an option";
 	cout << "1 - Return Card";
 	cout << "2 - Withdraw Cash";
 	cout << "3 - Check your Balance";
+	cin >> Menu;
 
-	switch (num)
-		case 1;
-	cout << "Return Card"
-		returnCard();
-
-	case 2;
-		cout << "Withdraw Cash"
-			WithdrawMoney()
-
-			case 3;
-		cout << "Check Balance"
-
-			// Add Invalid Input
-
+	switch (Menu) {
+	case 1:
+		cout << "Return Card";
+		ReturnCard();
+	case 2:
+		cout << "Withdraw Cash";
+		WithdrawMoney();
+	case 3:
+		cout << "Check Balance";
+		AccountBalance();
+	default:
+		cout << "Please select an option betwen 1 2 or 3";
+	}
 }
 
-void ReturnCard()
+void Thread::ReturnCard()
 {
+	//TODO: Implement GUI relevant card
 	cout << "Thank you for using Generic Bank";
 	//ReturnCard
 }
 
-void WithdrawMoney()
+void Thread::AccountBalance()
 {
-	int num;
+	//TODO: Implement GUI relevant card
+	cout << "Your Account balance is;";
+		cout << StoreBalance;
+	StageMenu();
+}
+
+void Thread::WithdrawMoney()
+{
+	//TODO: Implement GUI relevant card
+	//TODO: Why the fuck is switch is fucked again
 
 	cout << "How much should be Withdrawn";
 	cout << "£10";
@@ -93,37 +111,52 @@ void WithdrawMoney()
 	cout << "£100";
 	cout << "£500";
 
-	switch (num)
-		case 1;
-	cout << "£10"
-		returnCard();
+	switch (MenuMoney) {
+	case 1:
+		cout << "£10";
+		WithdrawAmmount = 10;
+		BalanceWithdraw();
 
-	case 2;
-		cout << "£20"
-			WithdrawMoney()
+	case 2:
+		cout << "£20";
+		WithdrawAmmount = 20;
+		BalanceWithdraw();
 
-			case 3;
-		cout << "£40"
+	case 3:
+		cout << "£40";
+		WithdrawAmmount = 40;
+		BalanceWithdraw();
 
-			case 3;
-		cout << "£100"
+	case 4:
+		cout << "£100";
+		WithdrawAmmount = 100;
+		BalanceWithdraw();
 
-			case 4;
-		cout << "£500"
+	case 5:
+		cout << "£500";
+		WithdrawAmmount = 500;
+		BalanceWithdraw();
+
+	default:
+	cout << "Please select an option from the Menu";
 
 			// Add Invalid Input and chose
-
-
-
+	}
 }
 
-void BalanceWithdraw()
+void Thread::BalanceWithdraw()
 {
 	//Check Balance and withdarw money
-	if (StoreBalance < Withdraw)
+	if (WithdrawAmmount > StoreBalance)
 	{
-
+		cout << "You do not have sufficent funds to withdraw cash";
+		StageMenu();
 	}
 
 	else
-}#include "Thread.h"
+	{
+		cout << "Withdrawing Cash";
+		cout << "Thank you for using Generic High Street Bank";
+		StageMenu();
+	}
+}
