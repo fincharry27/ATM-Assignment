36e79f430aedbc6d98b59f1875530948f995eb77
fincharry27 <harry.gfinch@gmail.com>
2020-04-06 12:55:43 +0100

    Added bank class and added methods to the main window form

---
+++ ATMSimulatorAssignment/Bank.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMSimulatorAssignment
+{
+    public class Bank
+    {
+        //fields
+        public Account[] accountList = new Account[3];
+
+        //methods
+        //Initialises accounts
+        public Bank()
+        {
+            accountList[0] = new Account(111111, 1111, 300);
+            accountList[0] = new Account(222222, 2222, 750);
+            accountList[0] = new Account(333333, 3333, 3000);
+        }
+
+        //returns list of accounts
+        public Account[] getAccountList()
+        {
+            return accountList;
+        }
+    }
+}
--- ATMSimulatorAssignment/MainWindow.cs
+++ ATMSimulatorAssignment/MainWindow.cs
@@ -12,9 +12,12 @@ namespace ATMSimulatorAssignment
 {
     public partial class MainWindow : Form
     {
-        public MainWindow()
+        public Account[] accountList;
+
+        public MainWindow(Account [] accountList)
         {
             InitializeComponent();
+            this.accountList = accountList;
         }
 
         private void optionsToolStripMenuItem_Click(object sender, EventArgs e)
@@ -24,7 +27,7 @@ namespace ATMSimulatorAssignment
 
         private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            MessageBox.Show("Assignment by Harry Finch and Elliot Kinkhead, for AC22005");
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
@@ -34,17 +37,21 @@ namespace ATMSimulatorAssignment
 
         private void DataSafeCheckBox_CheckedChanged(object sender, EventArgs e)
         {
-
+            bool ticked = ((CheckBox)sender).Checked;
+            for (int i = 0; i < accountList.Length; i++)
+            {
+                accountList[i].setDatasafe(ticked);
+            }
         }
 
         private void LaunchButton_Click(object sender, EventArgs e)
         {
-
+            //Add function for launching the atm system
         }
 
         private void ExitButton_Click(object sender, EventArgs e)
         {
-
+            Application.Exit();
         }
     }
 }
